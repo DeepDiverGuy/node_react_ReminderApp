f8ef653fe3ae5c361266b7ba47032e6161de52c1
Naymul Islam <naymulislam01855@gmail.com>
2022-11-11 00:59:15 +0600

    fixed typo

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div>
       <br />
-      <h3> <b> About This App: </b> </h3> <span style={{color:"#ede0e0"}}> This is a simple React App created with Node as a hobby project. This App has 'states' that remember then Reminders. You can add a reminder clicking the 'ADD' button. Also you can Delete those easily just by clicking the 'x' button. You can also switch notifications 'on' or 'off' by clicking on a remider twice! Lastly, you can click the "finished?" button when you don't need it.</span>
+      <h3> <b> About This App: </b> </h3> <span style={{color:"#ede0e0"}}> This is a simple React App created with Node as a hobby project. This App has 'states' that remember the Reminders. You can add a reminder clicking the 'ADD' button. Also you can Delete those easily just by clicking the 'x' button. You can also switch notifications 'on' or 'off' by clicking on a remider twice! Lastly, you can click the "finished?" button when you don't need it.</span>
       <br />
       <br />
       <hr />
